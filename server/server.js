cceab5de9b1a55af75ec6a055d0aa74ba9948dce
Kathiravan <kathiravanvellaisamy@gmail.com>
2025-12-09 15:43:46 +0530

    created prettier file for code fomating

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
-import express from 'express'
+import express from 'express';
 
-const app = express ()
+const app = express();
 
-const PORT = 5001
+const PORT = 5001;
 
-app.get("/api/notes",(req,res)=>{
-    res.json({ message:"you got 5 notes" })
-})
+app.get('/api/notes', (req, res) => {
+  //Get all the Posts
+  res.json({ message: 'you got 5 notes' });
+});
 
-app.listen(PORT,()=>{
-    console.log(`Application is running on port ${PORT}`);
-    
-})
+app.post('/api/notes', (req, res) => {
+  //craete a Post
+  res.json({ message: 'Note created successfully!' });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  //Update a Post
+  res.json({ message: 'Post updated successfully!' });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  //Delete a Post
+  res.json({ message: 'Post deleted successfully!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Application is running on port ${PORT}`);
+});
