dd9f89b7192108c471f635e8ed0bde5d265dd285
Kathiravan <kathiravanvellaisamy@gmail.com>
2025-12-09 11:19:50 +0530

    installed tailwind css and added poppins font family

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ import express from 'express'
 
 const app = express ()
 
-const PORT = 5000 
+const PORT = 5001
+
+app.get("/api/notes",(req,res)=>{
+    res.json({ message:"you got 5 notes" })
+})
 
 app.listen(PORT,()=>{
     console.log(`Application is running on port ${PORT}`);
