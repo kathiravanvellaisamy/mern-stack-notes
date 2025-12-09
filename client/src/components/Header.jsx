8825e186d04da4e983a37096415a8d3f29733408
Kathiravan <kathiravanvellaisamy@gmail.com>
2025-12-09 12:21:07 +0530

    changed button style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className='flex items-center justify-between'>
                 <h1 className='text-2xl md:text-3xl text-slate-700/80 font-semibold'>DevNote<span className='text-slate-950 font-extrabold'>X</span></h1>
 
-                <button  className="px-6 py-2 rounded-full border-2 border-slate-100 bg-slate-100 text-slate-950 hover:bg-white hover:text-slate-950 transition duration-500">Create Note</button>
+                <button  className="px-6 py-2 rounded-full border-2 border-slate-100 bg-slate-100 text-slate-950 hover:bg-white hover:text-slate-950 transition duration-500 cursor-pointer">Create Note</button>
             </div>
         </nav>
     </header>
